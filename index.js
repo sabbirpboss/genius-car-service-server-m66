700587e44a84c1354f2eb60b703ab4479aea78f6
sabbirpboss <sabbir.ah799@gmail.com>
2022-05-08 11:05:02 +0600

    before jwt , updated

--- index.js
+++ index.js
@@ -25,6 +25,7 @@ async function run() {
   try {
     await client.connect();
     const serviceCollection = client.db("geniusCar").collection("service");
+    const orderCollection = client.db('geniusCar').collection('order');
 
     app.get("/service", async (req, res) => {
       const query = {};
@@ -54,6 +55,24 @@ async function run() {
       const result = await serviceCollection.deleteOne(query);
       res.send(result);
     });
+    
+    // order collection API
+
+    app.get('/order', async(req, res) => {
+      const email = req.query.email;
+      const query = {email: email};
+      const cursor = orderCollection.find(query);
+      const orders = await cursor.toArray();
+      res.send(orders);
+    })
+
+    app.post('/order', async(req, res) => {
+      const order = req.body;
+      const result = await orderCollection.insertOne(order);
+      res.send(result);
+    })
+
+
   } finally {
   }
 }
